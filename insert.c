3cec7cd334bf705d6c9a998d49631350c4c4891b
KARRI HARSHA VARDHAN <89074728+karriharshavardhan@users.noreply.github.com>
2022-07-11 17:42:09 +0530

    optimized

--- insert.c
+++ insert.c
@@ -1,27 +1,36 @@
-    if(head==NULL){
-        head=temp;
-    }
-    else if(head->next==NULL){
-        if(strcmp(head->name,temp->name))
-    }
-    else{
-        int i=0,j=0;
-        if(p->name[i]<temp->name[j]){
-            p=p->next;
+void save(struct node* p){
+    struct node* temp; //to store contact info
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->name=(char*)malloc(20*sizeof(char)); //assuming string has a max size of 20
+    temp->pre=temp->next=NULL;
+    printf("enter the name of the contact: ");
+    scanf("%s",temp->name);
+    printf("enter the contact number of %s: ",temp->name);
+    scanf("%lld",&temp->number);
+    if(check_duplicate(head,temp)){
+        if(head==NULL){
+            head=temp;
         }
-        else if(p->name[i]==temp->name[j]){
-            i++;j++;
+        else if(strcmp(head->name,temp->name)>0){
+            head->pre=temp;
+            temp->next=head;
+            head=temp;
         }
         else{
-            if(p->next==NULL){
-                temp->pre=p;
-                p->next=temp;
+            struct node* r=NULL;
+            while(p && (strcmp(p->name,temp->name)<=0)){
+                r=p;
+                p=p->next;
             }
-            else{
-                temp->pre=p;
-                temp->next=p->next;
-                p->next->pre=temp;
-                p->next=temp;
+            temp->pre=r;
+            temp->next=p;
+            r->next=temp;
+            if(p!=NULL){
+                p->pre=temp;
             }
         }
     }
+    else{
+        printf("similar contact information is found in the contact list\n");
+    }
+}
