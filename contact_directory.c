aeae67a378b14cbd272dbfc912a4f4aeaf626935
KARRI HARSHA VARDHAN <89074728+karriharshavardhan@users.noreply.github.com>
2022-07-12 10:48:45 +0530

    updated

--- contact_directory.c
+++ contact_directory.c
@@ -16,22 +16,13 @@ void display(struct node* p){
     }
 }
 
-int compare(){
-
-}
-
-int check_duplicate(struct node* p,struct node* temp){ //to check whether a contanct with similar name or number exists
+int check_duplicate(struct node* p,struct node* temp){
     if(head==NULL){
         return 1;
     }
     else{
-        while(p){
-            if((strcmp(p->name,temp->name)) && (p->number!=temp->number)){
-                p=p->next;
-            }
-            else{
-                break;
-            }
+        while(p && (strcmp(p->name,temp->name)) && (p->number!=temp->number)){
+            p=p->next;
         }
         if(p==NULL){
             return 1;
@@ -51,15 +42,29 @@ void save(struct node* p){
     scanf("%s",temp->name);
     printf("enter the contact number of %s: ",temp->name);
     scanf("%lld",&temp->number);
-    //printf("%d\n",temp->name[0]);
-    //return;
     if(check_duplicate(head,temp)){
         if(head==NULL){
             head=temp;
         }
-        else{
-            
+        else if(strcmp(head->name,temp->name)>0){
+            head->pre=temp;
+            temp->next=head;
+            head=temp;
         }
+        else{
+            struct node* r=NULL;
+            while(p && (strcmp(p->name,temp->name)<=0)){
+                r=p;
+                p=p->next;
+            }
+            temp->pre=r;
+            temp->next=p;
+            r->next=temp;
+            if(p!=NULL){ //will face a segmentation error if this if statement is absent
+                p->pre=temp;
+            }
+        }
+        printf("contact '%s' saved successfully!!\n",temp->name);
     }
     else{
         printf("similar contact information is found in the contact list\n");
@@ -82,9 +87,9 @@ int main()
 {
     int opr;
     do{
-    printf("\nenter the operation you want to perform:");
     printf("\n1.Display all the contacts\n2.Save a new contact\n3.Edit an existing contact\n4.Delete a contact\n5.Search for a contact\n");
     printf("enter -1 to exit\n");
+    printf("enter the operation you want to perform:");
     scanf("%d",&opr);
         switch(opr){
         case 1:
@@ -111,7 +116,6 @@ int main()
         break;
     }
     }while(opr!=-1);
-
-
+    
     return 0;
 }
