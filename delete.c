2de21e7b4e7ae1d8b42dfd2ce05a563ba5dd5cc1
KARRI HARSHA VARDHAN <89074728+karriharshavardhan@users.noreply.github.com>
2022-07-17 14:25:10 +0530

    Update delete.c

--- delete.c
+++ delete.c
@@ -1,17 +1,17 @@
 void delete(struct node* p){
     if(head==NULL){
-        printf("\n**contact list is empty**\n");
+        printf("\n*contact list is empty*\n");
     }
     else{
         char* name;
-        name=(char*)malloc(100*sizeof(char));
+        name=(char*)malloc(20*sizeof(char));
         printf("enter the name of contact to be deleted: ");
         scanf("%s",name);
         while(strcmp(p->name,name)!=0){
             p=p->next;
         }
         if(p==NULL){
-            printf("\n**contact '%s' is not found**\n",name);
+            printf("\n*contact '%s' is not found*\n",name);
         }
         else{
             if(strcmp(head->name,name)==0){
@@ -26,6 +26,6 @@ void delete(struct node* p){
             }
             free(p);
         }
-        printf("\n**contact '%s' deleted successfully**\n",name);
+        printf("\n*contact '%s' deleted successfully*\n",name);
     }
 }
