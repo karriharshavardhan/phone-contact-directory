a3f208499831c0e8c747b559079f57e9e2db1449
Sharath <87656483+Sharath133@users.noreply.github.com>
2022-07-17 18:31:13 +0530

    Update contact_directory.c

--- contact_directory.c
+++ contact_directory.c
@@ -10,23 +10,6 @@ struct node{
     struct node* next;
 }*head=NULL;
 
-int check_duplicate(struct node* p,struct node* temp){
-    if(head==NULL){
-        return 1;
-    }
-    else{
-        while(p && (strcmp(p->name,temp->name)) && (p->number!=temp->number) && (strcmp(p->MailId,temp->MailId))){
-            p=p->next;
-        }
-        if(p==NULL){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-}
-
 void display(struct node* p){
     if(head==NULL){
         printf("\n*contact list is empty*\n");
